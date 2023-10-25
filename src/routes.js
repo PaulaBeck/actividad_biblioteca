0cd13a866e2d27c647e6d484daa5ce05c341ee45
PaulaBeck <beck.anapau@gmail.com>
2023-10-24 21:47:55 -0300

    Codigo fuente modificado con las nuevas funcionalidades

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,8 @@ import { libro } from './controller.js';
 
 export const router = Router()
 
-router.get('/libros', libro.getAll);
-router.get('/libro', libro.getOne);
+router.get('/libros', libro.getAll);        // obtener todos los registros que se tienen en la BD
+router.get('/libro', libro.getOne);         // obtener un registro determinado
+router.post('/libro', libro.add);           // agregar un registro a la BD
+router.put('/libro', libro.update);         // actualizar un registro existente
+router.delete('/libro', libro.delete);      // borrar un registro a partir del nro del isbn del libro
